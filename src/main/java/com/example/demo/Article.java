6fb29a489fe83b0479e277da3864a0a87ab3d442
insik03 <dbdlstlr25@naver.com>
2026-05-25 23:56:07 +0900

    feat : Article 파일 새로 생성

--- src/main/java/com/example/demo/Article.java
+++ src/main/java/com/example/demo/Article.java
@@ -1,14 +1,44 @@
 package com.example.demo;
 
 public class Article {
-    private String description;
+    private Long id;
+    private Long memberId;
+    private Long boardId;
+    private String title;
+    private String content;
+    private String date;
+    private String updateDate;
 
     public Article() {}
 
-    public Article(String description) {
-        this.description = description;
+    public Article(Long id, Long memberId, Long boardId, String title, String content, String date, String updateDate) {
+        this.id = id;
+        this.memberId = memberId;
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.updateDate = updateDate;
     }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getMemberId() { return memberId; }
+    public void setMemberId(Long memberId) { this.memberId = memberId; }
+
+    public Long getBoardId() { return boardId; }
+    public void setBoardId(Long boardId) { this.boardId = boardId; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getUpdateDate() { return updateDate; }
+    public void setUpdateDate(String updateDate) { this.updateDate = updateDate; }
 }
